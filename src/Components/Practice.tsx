d26393ac7288acc6c40a0cb38692e3e04cc863a3
daichi-nakano <nakano.daichi@i3design.co.jp>
2024-01-19 11:06:46 +0900

    コールシグネチャによる関数型の表現。引数の型をtype 変数で表すもの

--- src/Components/Practice.tsx
+++ src/Components/Practice.tsx
@@ -1,21 +1,16 @@
 const Practice = () => {
-  type User = {
-    name: string;
-    age: number;
+  //コールシグネチャによる関数型の表現
+  type MyF = {
+    isUsed?: boolean;
+    (arg: number): void;
   };
-  const users: User[] = [
-    { name: "jon", age: 7 },
-    { name: "adam", age: 60 },
-  ];
-  const i = users.map((u: User): string => u.name);
-  console.log(i);
-  //20歳以上のユーザーだけの配列
-  const adultUsers = users.filter((u: User) => u.age > 20);
-  //名前がjohnで始まるユーザーを探して返す
-  const jo = users.find((u: User) => u.name.startsWith("jon"));
-  console.log(jo);
-  console.log(adultUsers);
 
+  const double: MyF = (arg: number) => {
+    console.log(arg * 2);
+  };
+
+  double(3);
+  console.log(double.isUsed);
   return <div></div>;
 };
 
