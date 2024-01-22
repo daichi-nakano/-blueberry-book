e897445fac15f8ad065b5b06af72fa2d6752680b
daichi-nakano <nakano.daichi@i3design.co.jp>
2024-01-22 12:27:04 +0900

    メソッドは関数のように書くことができる

--- src/Components/Practice.tsx
+++ src/Components/Practice.tsx
@@ -2,6 +2,14 @@ const Practice = () => {
   class User {
     name?: string;
     age: number = 0;
+
+    isAdult = (): boolean => {
+      return this.age >= 20;
+    };
+
+    setAge(newAge: number) {
+      this.age = newAge;
+    }
   }
 
   const obj = {
@@ -10,6 +18,8 @@ const Practice = () => {
 
   const uhyo = new obj.cl();
   uhyo.name = "D";
+  console.log(uhyo.isAdult);
+  uhyo.setAge(26);
   console.log(uhyo.name);
   console.log(uhyo.age);
   return <div></div>;
